6a289d12ae2dcbc205fd623a90e733aec9664f1e
Ghost Liu <deluxghost@gmail.com>
2017-12-22 14:07:46 +0800

    update errors

--- ASF_IPC/error.py
+++ ASF_IPC/error.py
@@ -3,99 +3,99 @@
 
 class ASF_Error(Exception):
 
-    def __init__(self, value):
+    def __init__(self, message):
         super(ASF_Error, self).__init__()
-        self.value = value
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_BadRequest(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_BadRequest, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_BadRequest, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_Unauthorized(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_Unauthorized, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_Unauthorized, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_Forbidden(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_Forbidden, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_Forbidden, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_NotFound(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_NotFound, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_NotFound, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_NotAllowed(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_NotAllowed, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_NotAllowed, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_NotAcceptable(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_NotAcceptable, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_NotAcceptable, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_LengthRequired(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_LengthRequired, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_LengthRequired, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_InternalServerError(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_InternalServerError, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_InternalServerError, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
 
 
 class ASF_NotImplemented(ASF_Error):
 
-    def __init__(self, value):
-        super(ASF_NotImplemented, self).__init__(value)
-        self.value = value
+    def __init__(self, message):
+        super(ASF_NotImplemented, self).__init__(message)
+        self.message = message
 
     def __str__(self):
-        return self.value
+        return self.message
